a847345feed1f23a605719582e15b69e72964c34
Brian Sitati <briansit93@gmail.com>
2022-04-28 11:42:33 +0300

    completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <div>
+   <h1>please pass this test</h1> </div>;
 }
 
 export default Article;
+
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
